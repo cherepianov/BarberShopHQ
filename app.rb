47a3c008c22d176d5f852b3d65d4285d6ca719ca
Cherepianov <cherepianov@mail.ru>
2019-12-26 15:51:04 +0300

    save client information the cool way

--- app.rb
+++ app.rb
@@ -25,13 +25,24 @@ end
 
 post '/visit' do
 
-	@username = params[:username]
+  c = Client.new params[:client]
+  c.save
+
+=begin
+  @username = params[:username]
 	@phone = params[:phone]
 	@datetime = params[:datetime]
 	@barber = params[:barber]
 	@color = params[:color]
 
-
+  c = Client.new
+  c.name = @username
+  c.phone = @phone
+  c.datestamp = @datetime
+  c.barber = @barber
+  c.color = @color
+  c.save
+=end
 	erb "<h2>Спасибо, вы записались!</h2>"
 
 end
